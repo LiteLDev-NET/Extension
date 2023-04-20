fa3092a44c15f2d6eaedad561aaf4c6600910b96
Pd233 <minecraftpd233@163.com>
2023-04-20 22:25:24 +0800

    Init

---
+++ Minecraft.Extension/Minecraft.Extension/Global.cs
@@ -0,0 +1,22 @@
+﻿global using static Minecraft.Extension.Global;
+
+using LiteLoader.InterfaceAPI.Interop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minecraft.Extension;
+
+internal class Global
+{
+    public unsafe static nint DlsymRealThrowIfNull<T>(string symbol)
+        where T : Exception, new()
+    {
+        var ptr = Hook.dlsym_real(symbol);
+        if (ptr == null)
+            throw Activator.CreateInstance<T>();
+        return (nint)ptr;
+    }
+}
